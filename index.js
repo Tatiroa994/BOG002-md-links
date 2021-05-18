8014fb558d3f57d21fa3fdbffea3382e996ac4a2
Tatiroa994 <tatico994@gmail.com>
2021-05-18 14:44:12 -0500

    ajuste peticion http

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 const axios = require("axios");
 const marked = require("marked");
 const jsdom = require("jsdom");
-const { resolve } = require("path");
-const { rejects } = require("assert");
 
 const { JSDOM } = jsdom;
 
@@ -15,45 +13,61 @@ function getListLink(pathMd) {
   const tagsA = dom.window.document.querySelectorAll("a");
   tagsA.forEach((element) => {
     if (element.href.includes("http")) {
-      dataLinks.push({ text: element.textContent, link: element.href });
+      dataLinks.push({ href: element.href, text: element.textContent });
     }
   });
   return dataLinks;
 }
 
-function getStatusLink(link) {
-  return axios.get(link)
-    .then((result) => result)
-    .catch((error) => error);
+function createObjectValidate(objectLink, { status, statusText }) {
+  const { href, text, file } = objectLink;
+  const newObj = {
+    href,
+    text,
+    file,
+    status,
+    ok: statusText,
+  };
+  return newObj;
+}
+
+function getStatusLink(objectLink) {
+    return new Promise((resolve, reject) => {
+    axios.get(objectLink.href)
+      .then(({ status, statusText }) => {
+        resolve(createObjectValidate(objectLink, { status, statusText }));
+      })
+      .catch((error) => {
+        const status = error.response.status;
+        const statusText = error.response.statusText;
+        if (status) {
+          resolve(createObjectValidate(objectLink, { status, statusText }));
+        }
+        reject(error);
+      });
+  });
 }
 
 function mdLinks(pathLink, validate) {
-  // return new Promise(resolve, reject){
-  const linkList = getListLink(pathLink);
-  const arrayMdLinks = [];
-  linkList.forEach((element) => {
-    arrayMdLinks.push({
-      href: element.link,
-      text: element.text,
-      file: pathLink,
-    });
-  });
-  if (!validate) return arrayMdLinks;
-  arrayMdLinks.forEach((element) => {
-    const objCurrent = element;
-    getStatusLink(element.href)
-      .then((result) => {
-        objCurrent.status = result.status;
-        objCurrent.ok = result.statusText;
-      })
-      .catch((error) => {
-        objCurrent.status = error.response.status;
-        objCurrent.ok = error.response.statusText;
+  return new Promise((resolve, reject) => {
+    const arrayMdLinks = getListLink(pathLink)
+      .map((obLink) => ({ ...obLink, file: pathLink }));
+    if (!validate) {
+      resolve(arrayMdLinks);
+    } else {
+      const promises = [];
+      arrayMdLinks.forEach((element) => {
+        promises.push(getStatusLink(element));
       });
+      return Promise.all(promises).then((values) => resolve(values));
+    }
   });
-  return arrayMdLinks;
-
-// }
 }
 
-mdLinks('./README.md', { validate: true });
+mdLinks('./README.md', { validate: true })
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
+// mdLinks('./README.md')
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
